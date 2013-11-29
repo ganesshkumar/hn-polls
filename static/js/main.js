506a61bf1f2e130227cb48863a1676f12ad9e626
ganesshkumar <rpganesshkumar@gmail.com>
2013-11-30 03:14:34 +0530

    fixing for chrome

--- static/js/main.js
+++ static/js/main.js
@@ -9,6 +9,13 @@ make_clickable = function() {
             window.result = data.result;
             var canvas = document.getElementById(poll_id + "-canvas");
             canvas.style.display = "block";
+            options = {
+              scaleOverlay : true,
+              scaleOverride : true,
+              scaleStartValue : 0,
+              scaleSteps : 11,
+              scaleStepWidth : (Math.max.apply(Math, data.result.datasets.data)/10)
+            }
             new Chart(canvas.getContext("2d")).Bar(data.result);
         });
         return false;

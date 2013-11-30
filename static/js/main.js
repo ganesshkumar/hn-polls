5438c9457f4c51254b5cc3d095351456366e76d2
ganesshkumar <rpganesshkumar@gmail.com>
2013-12-01 02:52:33 +0530

    Adding refresh button

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,11 @@
 make_clickable = function() {
-    var poll_id = null
-    $('.orange-button').unbind("click").bind("click", function(){
+   show_button();
+   refresh_button();
+}
+
+show_button = function() {
+    var poll_id = null;
+    $('.show.button').unbind("click").bind("click", function(){
         poll_id = this.id.split("-")[0];
 
         button_text = $('#'+ poll_id +'-button')
@@ -8,80 +13,100 @@ make_clickable = function() {
 
         canvas = $('#' + poll_id + '-canvas');
         
-        if (canvas.css('display') != 'none') {
-            canvas.toggle("slide");
-            canvas.highcharts({});
+        if (canvas.css('display') == 'none') {
+            canvas.show()
+            if (canvas.html() != "") {
+                $('#' + poll_id + '-refresh').show();
+                return;
+            }
+        }
+        else {
+            $('#' + poll_id + '-refresh').hide();
+            canvas.hide();
             return;
         }
 
         $.getJSON($root_url + 'poll-detail', {
             poll_id: poll_id,
         }, function(data) {
-            window.result = data.result;
-            result = data.result;
-
-            //Code for highchart.js
-            canvas.highcharts({
-                chart: {
-                    type: 'bar',
-                    height: 200 + result.labels.length * 20,
-                },
-                title: {
-                    text: result.title
-                },
-                subtitle: {
-                 text: ''
-                },
-                xAxis: {
-                    categories: result.labels,
-                    title: {
-                        text: null
-                    }
-                },
-                yAxis: {
-                    min: 0,
-                    title: {
-                        text: 'Number of votes',
-                        align: 'high'
-                    },
-                    labels: {
-                        overflow: 'justify'
-                    }
-                },
-                tooltip: {
-                    valueSuffix: ' votes'
-                },
-                plotOptions: {
-                    bar: {
-                        dataLabels: {
-                            enabled: true
-                        }
-                    }
-                },
-                legend: {
-                    layout: 'vertical',
-                    align: 'right',
-                    verticalAlign: 'top',
-                    x: -40,
-                    y: 100,
-                    floating: true,
-                    borderWidth: 1,
-                    backgroundColor: '#FFFFFF',
-                    shadow: true
-                },
-                credits: {
-                    enabled: false
-                },
-                series: [{
-                    name: 'HN Votes',
-                    color: 'orange',
-                    data: result.votes
-                }]
-            });
-        $('#' + poll_id + '-canvas').toggle("slide");
-        $('#' + poll_id + '-canvas').width('100%');
-
+            set_highchart(canvas, poll_id, data.result);
+            $('#' + poll_id + '-refresh').show();
         });
-        return false;
-    })
+        return;
+    });
+}
+
+refresh_button = function () {
+    var poll_id = null;
+    $('.refresh.button').unbind("click").bind("click", function(){
+        poll_id = this.id.split("-")[0];
+        canvas = $('#' + poll_id + '-canvas');
+
+        $.getJSON($root_url + 'poll-detail', {
+            poll_id: poll_id,
+        }, function(data) {
+            set_highchart(canvas, poll_id, data.result);
+        });
+    });
+}
+
+set_highchart = function(element, poll_id, result) {
+    element.highcharts({
+        chart: {
+            type: 'bar',
+            backgroundColor: '#f6f6ef',
+            height: 200 + result.labels.length * 20,
+        },
+        title: {
+            text: result.title
+        },
+        subtitle: {
+            text: ''
+        },
+        xAxis: {
+            categories: result.labels,
+            title: {
+                text: null
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Number of votes',
+                align: 'high'
+            },
+            labels: {
+                overflow: 'justify'
+            }
+        },
+        tooltip: {
+            valueSuffix: ' votes'
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            x: -40,
+            y: 100,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor: '#FFFFFF',
+            shadow: true
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'HN Votes',
+            color: 'orange',
+            data: result.votes
+        }]
+    });
 }
